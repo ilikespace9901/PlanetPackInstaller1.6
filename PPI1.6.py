0e3f3b71fdeaaffcf488f882dcfe67a5d93deeea
ilikespace9901 <dvdpandzik@gmail.com>
2026-01-11 22:06:37 +0100

    Add files via upload

---
+++ PPI1.6.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+main = tk.Tk()
+main.title("Planet Pack Installer for 1.6")
+main.geometry("1200x700")
+
+label = tk.Label(main, text="PPI for 1.6", font=("Arial", 20))
+label.pack()
+
+main.mainloop()
