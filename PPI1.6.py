1cc0af480c7e45525eb7092733004177fc80d2cd
ilikespace9901 <dvdpandzik@gmail.com>
2026-01-13 07:29:27 +0100

    Add files via upload

--- PPI1.6.py
+++ PPI1.6.py
@@ -114,11 +114,9 @@ def create_buttons_from_descriptions():
             planet_desc = planet.get("description", "No description")
             planet_fileSize = planet.get("size", "Unknown file size")
             planet_version = planet.get("version", "Unknown version")
+            compat = planet.get("compat", "Unknown compatible versiom")
             
-            # 1.6 compat to be done, im too lazy now
-            
-            button_text = f"{planet_name} by {planet_author}\n Description: {planet_desc} \n size: {planet_fileSize}, version: {planet_version}"
-
+            button_text = f"{planet_name} by {planet_author}\nDescription: {planet_desc}\nsize: {planet_fileSize}, version: {planet_version}\ncompatible with version {compat}"
             button = ttk.Button(ModsFrame, style="Mods.TButton", text=button_text)
             button.pack(side="top", fill="x", pady=5)
 
